8b3452a70d8c1e4fea3840e3bb9c991a9be7b392
Олег Горбунов <gorbunov@alvic.local>
2020-02-05 17:51:11 +0300

    module3-task2 v.1.1

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var MOCK_COMMENTS = [
 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-// var MOCK_COMMENTS [
+// var MOCK_DESCRIPTIONS [
 // ];
 
 var NUMBER_OF_PHOTOS = 25;
@@ -24,4 +24,52 @@ var pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-var commentsTemplate = document.querySelector('.social_comment').cloneNode(true);
+var commentsTemplate = document.querySelector('.social__comment').cloneNode(true);
+
+var getАrbitraryIntFromInterval = function (min, max) {
+	return Math.floor(Math.random() * (max + 1 - min) + min);
+};
+
+var getАrbitraryFromArray = function (array) {
+	return array[getАrbitraryIntFromInterval(0, array.length - 1)];
+};
+
+var getАrbitraryComment = function () {
+	return (getАrbitraryIntFromInterval(1, 2) === 1) ? getАrbitraryFromArray(MOCK_COMMENTS) : getАrbitraryFromArray(MOCK_COMMENTS) + '' + getАrbitraryFromArray(MOCK_COMMENTS);
+};
+
+var buildItemsArrayWithGenerator = function (length, itemGenerator) {
+	var itemsArray = [];
+	for (var i = 0; i < length; i++) {
+		itemsArray[i]=itemGenerator(i)
+	}
+	return itemsArray;
+};
+
+var getАrbitraryPhoto = function (photoCounter) {
+	return {
+		url: 'photos/' + (photoCounter + 1) + '.jpg',
+		likes: getАrbitraryIntFromInterval(MIN_LIKES, MAX_LIKES),
+		comments: buildItemsArrayWithGenerator(getАrbitraryIntFromInterval(MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT), getАrbitraryComment),
+	};
+};
+
+var getPictureElement = function (picture) {
+  var pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = picture.url;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+  return pictureElement;
+};
+
+
+// Создание MOCK данных
+var photos = buildItemsArrayWithGenerator(NUMBER_OF_PHOTOS, getАrbitraryPhoto);
+
+// Создание и вставка на страницу превью изображений
+var photoElementsList = document.createDocumentFragment();
+for (var i = 0; i < NUMBER_OF_PHOTOS; i++) {
+  photoElementsList.appendChild(getPictureElement(photos[i]));
+}
+document.querySelector('.pictures').appendChild(photoElementsList);
+
