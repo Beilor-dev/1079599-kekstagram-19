5a29c8a83ff7d5f2a7a2c604d4569f86230eab0f
Олег Горбунов <gorbunov@alvic.local>
2020-03-06 11:25:17 +0300

    module7-task2 v.1.8

---
+++ js/upload.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function () {
+	var FILE_TYPES = ['jpg', 'jpeg', 'png', 'png', 'gif'];
+
+	var imgUploadFile = document.querySelector('#upload-file');
+	var previewUploadFile = document.querySelector('.img-upload__preview img');
+
+	imgUploadFile.addEventListener('change', function() {
+		var file = imgUploadFile.files[0];
+		var fileName = file.name.toLowerCase();
+
+		var matches = FILE_TYPES.some(function(it) {
+			return fileName.endsWith(it);
+		});
+		if(matches) {
+			var reader = new FileReader();
+			reader.addEventListener('load', function () {
+				previewUploadFile.src = reader.result;
+			});
+			reader.readAsDataURL(file);
+		};
+	});
+  // document.querySelector('#upload-file').addEventListener('change', function (evt) {
+  //  var file = evt.target.files[0];
+  //   var reader = new FileReader();
+  //   reader.addEventListener('load', function () {
+  //     document.querySelector('.img-upload__preview img').src = reader.result;
+  //   });
+  //   reader.readAsDataURL(file);
+  // });
+})();
+
+
