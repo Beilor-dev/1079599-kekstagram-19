1787e22ae61068d217bee07ac7957aec8b72fd3f
Олег Горбунов <gorbunov@alvic.local>
2020-03-18 13:56:26 +0300

    final v.2

--- js/validation.js
+++ js/validation.js
@@ -58,7 +58,7 @@
         evt.target.setCustomValidity('Хэш-тег не может состоять только из одной решётки');
         setupRedBorder(evt);
         return;
-      } 
+      }
       if (!hashtag.match(/^#[0-9a-zа-я]+$/)) {
         evt.target.setCustomValidity('Строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т.п.), символы пунктуации (тире, дефис, запятая и т.п.), эмодзи и т.д.');
         setupRedBorder(evt);
@@ -73,12 +73,12 @@
         evt.target.setCustomValidity('Нельзя указывать больше пяти хэш-тегов!');
         setupRedBorder(evt);
         return;
-      }  
+      }
       if (findMatchingElements(hashtagsArray)) {
         evt.target.setCustomValidity('Один хештег не может быть использован дважды');
         setupRedBorder(evt);
         return;
-      }        
+      }
     });
   });
 
